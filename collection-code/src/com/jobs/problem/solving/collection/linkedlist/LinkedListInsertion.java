12faad6d605c3f9a11e550d185877248ffbce08e
admin <admin@DESKTOP-82POU4M>
2021-01-31 09:00:03 +0530

    insert element at specified index

---
+++ collection-code/src/com/jobs/problem/solving/collection/linkedlist/LinkedListInsertion.java
@@ -0,0 +1,24 @@
+//Write a Java program to insert the specified element at the specified position in the linked list.
+package com.jobs.problem.solving.collection.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListInsertion {
+
+	public static void main(String[] args) {
+		List<String> linkedList = new LinkedList<String>();
+		// use add() method to add values in the linked list
+		linkedList.add("Red");
+		linkedList.add("Green");
+        linkedList.add("Black");
+        linkedList.add("White");
+        linkedList.add("Pink");
+        linkedList.add("Yellow");
+        
+        System.out.println("Original List :" + linkedList);
+        linkedList.add(3, "Blue");
+        System.out.println("List after insert the element at the specified position 3 :" + linkedList);
+        
+	}
+}
