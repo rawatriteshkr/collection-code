1aa77d2c6255011e98e90e5650417812b476c6f8
admin <admin@DESKTOP-82POU4M>
2021-01-31 09:04:09 +0530

    add element at first and last position

--- collection-code/src/com/jobs/problem/solving/collection/linkedlist/LinkedListInsertion.java
+++ collection-code/src/com/jobs/problem/solving/collection/linkedlist/LinkedListInsertion.java
@@ -2,23 +2,27 @@
 package com.jobs.problem.solving.collection.linkedlist;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class LinkedListInsertion {
 
 	public static void main(String[] args) {
-		List<String> linkedList = new LinkedList<String>();
+		LinkedList<String> linkedList = new LinkedList<String>();
 		// use add() method to add values in the linked list
 		linkedList.add("Red");
 		linkedList.add("Green");
-        linkedList.add("Black");
-        linkedList.add("White");
-        linkedList.add("Pink");
-        linkedList.add("Yellow");
-        
-        System.out.println("Original List :" + linkedList);
-        linkedList.add(3, "Blue");
-        System.out.println("List after insert the element at the specified position 3 :" + linkedList);
-        
+		linkedList.add("Black");
+		linkedList.add("White");
+		linkedList.add("Pink");
+		linkedList.add("Yellow");
+
+		System.out.println("Original List :" + linkedList);
+		linkedList.add(3, "Blue");
+		System.out.println("List after insert the element at the specified position 3 :" + linkedList);
+		// add element at first position
+		linkedList.addFirst("Brown");
+		System.out.println("List after insert the element at the first position :" + linkedList);
+		// add element at last position
+		linkedList.addLast("Brown");
+		System.out.println("List after insert the element at the last position :" + linkedList);
 	}
 }
